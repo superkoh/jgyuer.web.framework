a66ac068b11e48d9c71383cdec717c89e6f6d92f
王乐天 <koh.718@gmail.com>
2017-02-07 00:35:50 +0800

    mybatis generator tool

--- tools/mbg/mbg.sh
+++ tools/mbg/mbg.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp ${DIR}/mysql-connector-java-5.1.38.jar:${DIR}/mybatis-generator-core-1.3.3.jar org.mybatis.generator.api.ShellRunner "$@"
+mkdir -p ${HOME}/.jgyuer/libs/java
+MYSQL_CONNECTOR_FILE="$HOME/.jgyuer/libs/java/mysql-connector-java-5.1.40.jar"
+MYBATIS_GENERATOR_FILE="$HOME/.jgyuer/libs/java/mybatis-generator-core-1.3.3.jar"
+
+if [ -f "$MYSQL_CONNECTOR_FILE" ]
+then
+	echo "$MYSQL_CONNECTOR_FILE found."
+else
+	echo "$MYSQL_CONNECTOR_FILE not found."
+    echo "downloading missing dependency"
+    curl --output ${MYSQL_CONNECTOR_FILE} http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.40/mysql-connector-java-5.1.40.jar
+fi
+
+if [ -f "$MYBATIS_GENERATOR_FILE" ]
+then
+	echo "$MYBATIS_GENERATOR_FILE found."
+else
+	echo "$MYBATIS_GENERATOR_FILE not found."
+    echo "downloading missing dependency"
+    curl --output ${MYBATIS_GENERATOR_FILE} http://central.maven.org/maven2/org/mybatis/generator/mybatis-generator-core/1.3.3/mybatis-generator-core-1.3.3.jar
+fi
+
+java -cp ${MYSQL_CONNECTOR_FILE}:${MYBATIS_GENERATOR_FILE} org.mybatis.generator.api.ShellRunner "$@"
